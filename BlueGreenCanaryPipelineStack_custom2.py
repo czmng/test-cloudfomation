909dcaacc1f0a2a5c245539c2a52ec2331f17787
Chark <2405302553@qq.com>
2025-06-08 00:19:35 +0800

    add codedeploy

--- BlueGreenCanaryPipelineStack_custom2.py
+++ BlueGreenCanaryPipelineStack_custom2.py
@@ -8,9 +8,14 @@ from aws_cdk import (
     aws_autoscaling as autoscaling,
     aws_elasticloadbalancingv2 as elbv2,
     aws_elasticloadbalancingv2_targets as elbv2_targets,
+    aws_codedeploy as codedeploy,
+    aws_s3 as s3,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as codepipeline_actions,
     Stack,
     Stage,
     Environment,
+    RemovalPolicy
 )
 from constructs import Construct
 from aws_cdk import pipelines as cdk_pipelines
@@ -48,20 +53,30 @@ class BlueGreenCanaryDemoStack(Stack):
             self, "SSM-Role", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
             managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore")]
         )
-        self.db_master = rds.DatabaseInstance(
-            self, "RDS-MySQL-Master", engine=rds.DatabaseInstanceEngine.mysql(version=rds.MysqlEngineVersion.VER_8_0_35),
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL),
-            vpc=self.vpc, vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
-            security_groups=[db_sg], multi_az=True, database_name="prodDB",
-            credentials=rds.Credentials.from_generated_secret("ProdDBAdmin"),
-        )
+        # self.db_master = rds.DatabaseInstance(
+        #     self, "RDS-MySQL-Master", engine=rds.DatabaseInstanceEngine.mysql(version=rds.MysqlEngineVersion.VER_8_0_35),
+        #     instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL),
+        #     vpc=self.vpc, vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
+        #     security_groups=[db_sg], multi_az=True, database_name="prodDB",
+        #     credentials=rds.Credentials.from_generated_secret("ProdDBAdmin"),
+        # )
 
 
         windows_ami = ec2.MachineImage.latest_windows(
             ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE
         )
 
-
+        user_data = ec2.UserData.for_windows()
+        user_data.add_commands(
+            "mkdir C:\\temp",
+            # Download & install CodeDeploy agent
+            "Invoke-WebRequest -Uri https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/codedeploy-agent.msi -OutFile C:\\temp\\codedeploy-agent.msi",
+            "Start-Process -FilePath msiexec.exe -ArgumentList '/i \"C:\\temp\\codedeploy-agent.msi\" /quiet' -Wait",
+            "Start-Service codedeployagent",
+            # Install IIS
+            "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command \"Install-WindowsFeature Web-Server, Web-Mgmt-Tools, Web-Scripting-Tools, Web-Mgmt-Console, Web-Mgmt-Service -IncludeAllSubFeature -IncludeManagementTools\"",
+            "Start-Service W3SVC",
+        )
         instance_blm = ec2.Instance(
             self, "Instance-Prod-BLM",
             vpc=self.vpc,
@@ -83,6 +98,7 @@ class BlueGreenCanaryDemoStack(Stack):
             min_capacity=2,
             max_capacity=4,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
+            user_data=user_data,
         )
 
 
@@ -96,6 +112,7 @@ class BlueGreenCanaryDemoStack(Stack):
             min_capacity=2,
             max_capacity=4,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
+            user_data=user_data,
         )
 
 
@@ -137,7 +154,101 @@ class BlueGreenCanaryDemoStack(Stack):
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
             key_name=JUMPBOX_KEY_NAME,
         )
+        
+        # CodeDeploy application & deployment group (canary)
+        app = codedeploy.ServerApplication(self, "CodeDeployApp", application_name="DemoApp")
+        codedeploy_role = iam.Role(
+            self, "CodeDeployServiceRole", assumed_by=iam.ServicePrincipal("codedeploy.amazonaws.com")
+        )
+        codedeploy_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2FullAccess")
+        )
+        codedeploy_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess")
+        )
 
+        deployment_group = codedeploy.ServerDeploymentGroup(
+            self,
+            "DeploymentGroup",
+            application=app,
+            auto_scaling_groups=[asg_bis],
+            deployment_config=codedeploy.ServerDeploymentConfig.ONE_AT_A_TIME,
+            load_balancers=[
+                codedeploy.LoadBalancer.application(tg_bis),
+                codedeploy.LoadBalancer.application(tg_bis_fence),
+            ],
+            role=codedeploy_role,
+        )
+
+        pipeline_role = iam.Role(
+            self,
+            "WebAppPipelineRole",
+            assumed_by=iam.ServicePrincipal("codepipeline.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodePipeline_FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeDeployFullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+            ]
+        )
+
+        # CodePipeline Artifact Store
+        artifact_store = s3.Bucket(
+            self,
+            "PipelineArtifacts",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        # Source Artifact
+        source_output = codepipeline.Artifact("SourceOutput")
+
+        deployment_bucket = s3.Bucket(
+            self,
+            "DeploymentBucket",
+            bucket_name="app-pipeline-2025-23",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+        
+        self.deployment_bucket = deployment_bucket
+
+
+
+        
+        # 创建webapp CodePipeline
+        webapp_pipeline = codepipeline.Pipeline(
+            self,
+            "WebAppPipeline",
+            pipeline_name="webapp",
+            role=pipeline_role,
+            artifact_bucket=artifact_store,
+            stages=[
+                # Source Stage - 监控S3存储桶
+                codepipeline.StageProps(
+                    stage_name="Source",
+                    actions=[
+                        codepipeline_actions.S3SourceAction(
+                            action_name="S3Source",
+                            bucket=deployment_bucket,
+                            bucket_key="app.zip",
+                            output=source_output,
+                            trigger=codepipeline_actions.S3Trigger.EVENTS,  # 使用CloudWatch Events触发
+                        )
+                    ]
+                ),
+                # Deploy Stage - 使用CodeDeploy
+                codepipeline.StageProps(
+                    stage_name="Deploy",
+                    actions=[
+                        codepipeline_actions.CodeDeployServerDeployAction(
+                            action_name="CodeDeploy",
+                            input=source_output,
+                            deployment_group=deployment_group,
+                        )
+                    ]
+                )
+            ]
+        )
 class BlueGreenCanaryDemoStage(Stage):
     """Wraps the ProdStack in a stage so it can be deployed by the pipeline."""
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
